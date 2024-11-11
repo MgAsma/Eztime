7066b828fd727e2055157490adee22395ecd0ad3
MgAsma <asma@ekfrazo.in>
2024-11-11 21:08:15 +0530

    holiday calendar ui and api integration done

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -62,7 +62,11 @@ export const environment = {
   update_timesheet_status:`update-timesheet-status`,
   update_leave_details:`update-leave-details`,
   allEmployee:`all-employee`,
-  working_hour:`working-hour`
+  working_hour:`working-hour`,
+  designation:`designation`,
+  department:`department`,
+  user:`user`,
+  holiday_calender:`upload-holiday-calender`
 };
 
 /*
