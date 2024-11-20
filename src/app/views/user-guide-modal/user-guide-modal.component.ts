cedf79acbbd8e3096ab585f77ace9815a1d7a2a4
Arpitha-Gowri <143486020+22arpitha@users.noreply.github.com>
2024-11-20 13:05:35 +0530

    user guide images added to employee and admin roles

--- src/app/views/user-guide-modal/user-guide-modal.component.ts
+++ src/app/views/user-guide-modal/user-guide-modal.component.ts
@@ -9,12 +9,58 @@ import { BsModalRef } from 'ngx-bootstrap/modal';
 export class UserGuideModalComponent implements OnInit {
   showIndicator = true;
   noWrapSlides = true;
+  imageSlides: any = []
   constructor(
     public bsModalRef: BsModalRef
   ) { }
 
   ngOnInit(): void {
+    this.carouselImages();
   }
+  carouselImages() {
+    if (sessionStorage.getItem('user_role_name') === 'Admin') {
+      this.imageSlides = [
+        { image: '../../../assets/images/Screenshots/1.png' },
+        { image: '../../../assets/images/Screenshots/2.png' },
+        { image: '../../../assets/images/Screenshots/3.png' },
+        { image: '../../../assets/images/Screenshots/4.png' },
+        { image: '../../../assets/images/Screenshots/5.png' },
+        { image: '../../../assets/images/Screenshots/6.png' },
+        { image: '../../../assets/images/Screenshots/7.png' },
+        { image: '../../../assets/images/Screenshots/8.png' },
+        { image: '../../../assets/images/Screenshots/9.png' },
+        { image: '../../../assets/images/Screenshots/10.png' },
+        { image: '../../../assets/images/Screenshots/12.png' },
+        { image: '../../../assets/images/Screenshots/13.png' },
+        { image: '../../../assets/images/Screenshots/14.png' },
+        { image: '../../../assets/images/Screenshots/15.png' },
+        { image: '../../../assets/images/Screenshots/16.png' },
+        { image: '../../../assets/images/Screenshots/17.png' },
+        { image: '../../../assets/images/Screenshots/18-1.png' },
+        { image: '../../../assets/images/Screenshots/18.png' },
+        { image: '../../../assets/images/Screenshots/19.png' },
+        { image: '../../../assets/images/Screenshots/20.png' },
+        { image: '../../../assets/images/Screenshots/21.png' },
+        { image: '../../../assets/images/Screenshots/22.png' }
+      ];
+    } else if (sessionStorage.getItem('user_role_name') === 'SuperAdmin') {
+      this.imageSlides = [];
+    }
+    else if (sessionStorage.getItem('user_role_name') === 'Employee') {
+      this.imageSlides = [
+        { image: '../../../assets/images/Screenshots/9.png' },
+        { image: '../../../assets/images/Screenshots/10.png' },
+        { image: '../../../assets/images/Screenshots/12.png' },
+        { image: '../../../assets/images/Screenshots/13.png' },
+        { image: '../../../assets/images/Screenshots/14.png' },
+        { image: '../../../assets/images/Screenshots/15.png' },
+        { image: '../../../assets/images/Screenshots/16.png' },
+        { image: '../../../assets/images/Screenshots/17.png' },
+        { image: '../../../assets/images/Screenshots/18-1.png' },
+      ]
+    }
+  }
+
   close(): void {
     this.bsModalRef.hide();
   }
