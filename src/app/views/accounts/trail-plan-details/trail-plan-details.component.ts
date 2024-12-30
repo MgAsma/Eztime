25ac6c8dad701c3e1a3edcdfe1d680375ba88f5b
MgAsma <asma@ekfrazo.in>
2024-12-30 15:49:57 +0530

    Buy trail plan api integration done

--- src/app/views/accounts/trail-plan-details/trail-plan-details.component.ts
+++ src/app/views/accounts/trail-plan-details/trail-plan-details.component.ts
@@ -3,6 +3,7 @@ import { MatDialog } from '@angular/material/dialog';
 import { CommonServiceService } from '../../../service/common-service.service';
 import { BuyStandardplanComponent } from '../buy-standardplan/buy-standardplan.component';
 import { ExistStandardPlanComponent } from '../exist-standard-plan/exist-standard-plan.component';
+import { StandardSubscriptionComponent } from '../standard-subscription/standard-subscription.component';
 
 @Component({
   selector: 'app-trail-plan-details',
@@ -12,6 +13,10 @@ import { ExistStandardPlanComponent } from '../exist-standard-plan/exist-standar
 export class TrailPlanDetailsComponent implements OnInit {
 BreadCrumbsTitle:any='Subscription plan';
   monthly: boolean = true;
+  planDetails: any;
+  monthlyAmount: any;
+  yearlyAmount: any;
+  discount: any;
   constructor(
     private common_service:CommonServiceService,
     private dialog:MatDialog
@@ -19,6 +24,26 @@ BreadCrumbsTitle:any='Subscription plan';
 
   ngOnInit(): void {
     this.common_service.setTitle(this.BreadCrumbsTitle);
+    this.common_service.subscription_details.subscribe((res)=>{
+      this.planDetails = res;
+      console.log(res,"DETAILS")
+      res?.forEach((item: any,i) => {
+        console.log(item,"item")
+        if(item.name == 'Standard'){
+          item['subscription_deatils'].forEach((item: any,i) => {
+            if(item.yearly_or_monthly_name === 'Monthly'){
+              this.monthlyAmount = item.amount
+            }if(item.yearly_or_monthly_name === 'Yearly'){
+              this.yearlyAmount = item.amount
+              this.discount = item.discount
+            }
+          })
+          
+      }
+      })
+    
+    })
+  
   }
   setPaymentOption(option: boolean): void {
     this.monthly = option;
@@ -26,18 +51,25 @@ BreadCrumbsTitle:any='Subscription plan';
   cancelSubscription(){}
   addNewUser(){
     const dialogRef = this.dialog.open(ExistStandardPlanComponent, {
-      // data: { message: 'Hello from the parent component!' },
+      data: {planDetails:this.planDetails},
       panelClass: 'custom-dialog'
     });
     dialogRef.disableClose=true
   }
   renewSubscription(){
     const dialogRef = this.dialog.open(BuyStandardplanComponent, {
-      // data: { message: 'Hello from the parent component!' },
+      data: {planDetails:this.planDetails},
       panelClass: 'custom-dialog'
     });
     dialogRef.disableClose=true
      
   }
-
+  openStandardPlan(){
+    const dialogRef = this.dialog.open(StandardSubscriptionComponent, {
+      // data: {planDetails:this.planDetails},
+      panelClass: 'custom-dialog'
+    });
+    dialogRef.disableClose=true
+     
+  }
 }
