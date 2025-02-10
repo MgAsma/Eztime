9da3dbab026999632de76587ce9112b7c501fc27
Arpitha-Gowri <143486020+22arpitha@users.noreply.github.com>
2025-02-10 19:06:27 +0530

    small changes in roles and accessbilities

--- src/app/views/roles/access-to-modules/access-to-modules.component.ts
+++ src/app/views/roles/access-to-modules/access-to-modules.component.ts
@@ -2,6 +2,7 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ApiserviceService } from '../../../service/apiservice.service';
 import { concat } from 'rxjs';
+import { NgxUiLoaderService } from 'ngx-ui-loader';
 
 @Component({
   selector: 'app-access-to-modules',
@@ -25,7 +26,8 @@ export class AccessToModulesComponent implements OnInit {
   constructor(
     private activeRoute: ActivatedRoute,
     private api: ApiserviceService,
-    private router: Router
+    private router: Router,
+    private ngxService: NgxUiLoaderService,
   ) {
     this.designation_id = this.activeRoute.snapshot.paramMap.get('id')
   }
@@ -38,7 +40,8 @@ export class AccessToModulesComponent implements OnInit {
   }
   ngOnInit(): void {
     this.organization_id = sessionStorage.getItem('organization_id')
-
+    this.ngxService.stop();
+    this.ngxService.stopLoader("loader-01");
   }
 
 
@@ -202,7 +205,7 @@ export class AccessToModulesComponent implements OnInit {
         );
 
         if (allOperationsFalse && api_data?.length === 1) {
-          // console.log("All operations are false.");
+          console.log("All operations are false.");
           this.errorMsg = true;
           this.disableAddOrUpdateBtn = true;
         } else {
@@ -261,9 +264,12 @@ export class AccessToModulesComponent implements OnInit {
             element_sub_module['operations'][0][access_name] = event.target.checked;
             // console.log('false not view')
           }
+        } else{
+          element_sub_module['operations'][0][access_name] = event.target.checked
         }
       }
     });
+    console.log(this.data, this.storeAssessGivenData)
     this.checkSubModuleAccess(this.data, this.storeAssessGivenData);
   }
 
