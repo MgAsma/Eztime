6363ad44710e74d3d54dfaf75f16468fc466b783
Asma <asma@dataseedtech.com>
2025-02-12 16:08:22 +0530

    No-internet svg implemented

--- src/app/views/pages/no-internet/no-internet.component.ts
+++ src/app/views/pages/no-internet/no-internet.component.ts
@@ -8,13 +8,24 @@ import { Router } from '@angular/router';
 })
 export class NoInternetComponent implements OnInit {
   user_id: string;
+  imgUrl: any;
+  noInternetImage: string | null;
 
   constructor(
     private router:Router) { }
 
   ngOnInit(): void {
+    this.noInternetImage = sessionStorage.getItem('noInternetImage');
+    if (this.noInternetImage) {
+      this.preloadImage(this.noInternetImage);
+    }
   }
   goBack(){
     this.router.navigate(['/login'])
   }
+  preloadImage(url: string) {
+    this.imgUrl = new Image();
+    this.imgUrl['src'] = url;
+  }
+
 }
